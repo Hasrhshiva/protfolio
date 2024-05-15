4964c2b2e4dfb86a6068df7dc896cc2343515377
harshal110024 <harshalh@butomy.in>
2024-05-15 09:44:33 +0530

    email changes

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './contact.css';
 import ClientImg1 from '../../assets/profile-img.png';
 import Instagram from '../../assets/icons8-instagram-48.png';
 import Facebook from '../../assets/icons8-facebook-48.png';
 import Twitter from '../../assets/icons8-twitterx-50.png';
 import Linkedin from '../../assets/icons8-linkedin-48.png';
+import emailjs from '@emailjs/browser';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 const Contact = () => {
+  const [refresh, setRefresh] = useState(false); // State to trigger re-render
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_1zd02ru', 'template_7iqp5yi', form.current, {
+        publicKey: 'yxH5Ry97UFO_yGosc',
+      })
+      .then(
+        () => {
+          toast.success('Email sent successfully!');
+          setRefresh(!refresh);
+        },
+        (error) => {
+          toast.error(`Failed to send email: ${error.text}`);
+
+        },
+      );
+  };
+
   return (
         <section id='contactPage'>
             <div id='clients'>
@@ -27,9 +52,9 @@ const Contact = () => {
             <div id='contact'>
                   <h1 className='contactPageTitle'>Contact Me</h1>
                   <span className='contactDesc'>Please Fill out of gthis and send a email for me so good clients</span>
-                  <form className='contactForm'>
-                    <input type='text' placeholder='Your Name' className='name' />
-                    <input type='email' placeholder='Your Email' className='email' />
+                  <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                    <input type='text' placeholder='Your Name' className='name' name="your_name" />
+                    <input type='email' placeholder='Your Email' className='email' name="your_email" />
                     <textarea className='msg' placeholder='Your Message' rows="5" name="message" />
                     <button className='submitBtn'>Send Message</button>
                     <div className='links'>
